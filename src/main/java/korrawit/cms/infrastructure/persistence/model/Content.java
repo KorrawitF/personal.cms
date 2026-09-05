d2e27b99f8e17cac0e08c33e6037fe2d4014e40b
Korrawit Soodnalao <u6211548@au.edu>
2026-09-05 18:57:32 +0700

    feat: content index

--- src/main/java/korrawit/cms/infrastructure/persistence/model/Content.java
+++ src/main/java/korrawit/cms/infrastructure/persistence/model/Content.java
@@ -11,10 +11,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "contents")
+@Table(name = "contents", indexes = {
+        @Index(name = "idx_contents_page", columnList = "page")
+})
 public class Content {
 
     @Id
